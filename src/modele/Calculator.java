b7ad17b67dcbc6d32d8cfd3c7f8d4bede033329a
Madeline Carpentier <madeline8112@outlook.fr>
2019-09-20 15:00:40 +0200

    start

---
+++ src/modele/Calculator.java
@@ -0,0 +1,12 @@
+package modele;
+
+/**
+ * 20/09/2019
+ * @author carpentm
+ * Interface calculator permettant de faire des calcule dans d'autre classe
+ *
+ */
+public interface Calculator {
+	
+	public void CalculStepped();
+}
---
+++ src/modele/SteppedCalculator.java
@@ -0,0 +1,28 @@
+package modele;
+
+import java.util.Iterator;
+
+/**
+ * @Date 20/09/2019
+ * @author carpentm
+ *Class intérable 
+ */
+
+public class SteppedCalculator implements Iterator{
+	//Les attributs
+	protected Map<Entite, Trajectoire> liste;
+	
+	
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
