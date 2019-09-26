5dbdb9a9d99a0afb0ff4ae618329401617a9c1ca
Lefranc Néo <neo.lefranc@gmail.com>
2019-09-26 14:15:29 +0200

    entite et autre

---
+++ src/modele/Etoile.java
@@ -0,0 +1,11 @@
+package modele;
+
+import javafx.geometry.Point2D;
+
+public class Etoile extends Entite {
+
+	public Etoile(Point2D position, double masse, double rayon, String name) {
+		super(position, masse, rayon, name);
+	}
+	
+}
