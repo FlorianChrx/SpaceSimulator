1de07392182835119091c3a799cc8bb8a96564ce
Florian CHIRAUX <florian.chiraux.etu@univ-lille.fr>
2019-09-26 16:15:59 +0200

    Add Vecteur

--- src/modele/Entite.java
+++ src/modele/Entite.java
@@ -14,7 +14,10 @@ public abstract class Entite {
 		this.rayon = rayon;
 		this.name = name;
 	}
-
+	/**
+	 * renvoie la masse
+	 * @return renvoie la masse en double
+	 */
 	public double getMasse() {
 		return masse;
 	}
@@ -26,23 +29,38 @@ public abstract class Entite {
 	public void setPosition(Point position) {
 		this.position = position;
 	}
-
+	/**
+	 * renvoie le nom
+	 * @return renvoie le nom en String
+	 */
 	public String getName() {
 		return name;
 	}
-
+	/**
+	 * remplace le nom
+	 * @param name nouveau nom
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	/**
+	 * remplace la masse
+	 * @param masse nouvelle masse
+	 */
 	public void setMasse(double masse) {
 		this.masse = masse;
 	}
-
+	/**
+	 * renvoie le rayon
+	 * @return renvoie le rayon en double
+	 */
 	public double getRayon() {
 		return rayon;
 	}
-
+	/**
+	 * remplace le rayon
+	 * @param rayon nouveau rayon
+	 */
 	public void setRayon(double rayon) {
 		this.rayon = rayon;
 	}
