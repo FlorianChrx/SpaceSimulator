0f3b55f5a10f3c04eb59c13612b65f3b94562dd9
FlorianChrx <florianchiro@hotmail.fr>
2019-09-26 18:15:05 +0200

    quelques oublis dans Task/Service

--- src/controller/CalculTask.java
+++ src/controller/CalculTask.java
@@ -1,12 +1,25 @@
 package controller;
 
+import java.util.List;
+
 import javafx.concurrent.Task;
+import modele.Calculator;
+import modele.EntiteMobile;
 
 public class CalculTask extends Task<Void> {
+	
+	protected List<EntiteMobile> list;
+	protected Calculator calculator;
+
+	public CalculTask(List<EntiteMobile> list, Calculator calculator) {
+		super();
+		this.list = list;
+		this.calculator = calculator;
+	}
 
 	@Override
 	protected Void call() throws Exception {
-		// TODO Auto-generated method stub
+		calculator.CalculNextStep(list);
 		return null;
 	}
 
