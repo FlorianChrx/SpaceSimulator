6ddebde2e86d806c722f9272671ee6ea8868e82b
FlorianChrx <florianchiro@hotmail.fr>
2019-10-04 14:31:09 +0200

    Implementation de méthodes d'ajouts d'entités dans le Système solaire

--- src/modele/SystemeSolaire.java
+++ src/modele/SystemeSolaire.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class SystemeSolaire {
 	
+	protected Vaisseau vaisseau;
 	protected List<EntiteMobile> entityList = new ArrayList<EntiteMobile>();
 	protected Calculator calcul = new SteppedCalculator();
 	protected Point entityCenter = new Point(0,0);
@@ -36,7 +37,30 @@ public class SystemeSolaire {
 		return calcul;
 	}
 
+	public Vaisseau getVaisseau() {
+		return vaisseau;
+	}
+
+	public void setVaisseau(Vaisseau vaisseau) {
+		this.vaisseau = vaisseau;
+	}
+
+	public void setCalcul(Calculator calcul) {
+		this.calcul = calcul;
+	}
+
+	public void addVaisseau(Vaisseau v) {
+		setVaisseau(v);
+		entityList.add(v);
+	}
 	
+	public void add(EntiteMobile em) {
+		if(em instanceof Vaisseau) {
+			addVaisseau((Vaisseau) em);
+		} else {
+			entityList.add(em);
+		}
+	}
 	
 	
 }
