3e76e4f62480ef8b3345dc23f099c124ae295a3f
FlorianChrx <florianchiro@hotmail.fr>
2019-10-09 10:20:22 +0200

    test de forceBetween et modification liées

--- src/modele/Calculator.java
+++ src/modele/Calculator.java
@@ -11,4 +11,5 @@ import java.util.List;
 public interface Calculator {
 	public void CalculNextStep(List<EntiteMobile> list);
 	public void euleurExplicite(double tDebut, double tFin, EntiteMobile mobile);
+	public Vecteur forceBetween(Entite v1, Entite v2);
 }
