99115c47c8130f92544741ea2afb6d6d53496f54
Madeline CARPENTIER <madeline.carpentier.etu@univ-lille.fr>
2019-10-18 14:04:57 +0200

    Replace Calculator.java

--- src/modele/maths/Calculator.java
+++ src/modele/maths/Calculator.java
@@ -1,13 +1,14 @@
-package modele.maths;
-
-import modele.system.SystemeSolaire;
-
+package math;
 /**
  * 20/09/2019
  * @author carpentm
  * Interface calculator permettant de faire des calcule dans d'autre classe
  *
  */
+
+import system.EntiteMobile;
+import system.SystemeSolaire;
+
 public interface Calculator {
-	public void CalculNextStep(SystemeSolaire s);
+	public void CalculNextStep(SystemeSolaire sys, EntiteMobile selectionne);
 }
