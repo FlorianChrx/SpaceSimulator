82b0b2e8413b1ba064bf792410c94e387e1b6239
Madeline CARPENTIER <madeline.carpentier.etu@univ-lille.fr>
2019-10-18 14:03:57 +0200

    Replace Vecteur.java

--- src/modele/maths/Vecteur.java
+++ src/modele/maths/Vecteur.java
@@ -1,56 +1,70 @@
-package modele.maths;
+package math;
 
-import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 
+ * @author chirauf
+ * @Date ???
+ * 
+ */
+
 public class Vecteur {
-	protected double vitx;
-	protected double vity;
-	
+	protected double vitx; // vecteur en absysse, on suppose qu'on part de 0
+	protected double vity; // vecteur en ordonné, on suppose qu'on part de 0
+
 	public Vecteur(double vitx, double vity) {
 		super();
 		this.vitx = vitx;
 		this.vity = vity;
 	}
-	
+
 	public double getVitx() {
 		return vitx;
 	}
-	
+
 	public void setVitx(double vitx) {
 		this.vitx = vitx;
 	}
-	
+
 	public double getVity() {
 		return vity;
 	}
-	
+
 	public void setVity(double vity) {
 		this.vity = vity;
 	}
-	
+
 	public static Vecteur somme(Vecteur v1, Vecteur v2) {
-		return new Vecteur(v1.getVitx()+v2.getVitx(), v1.getVity()+v2.getVity());
+		return new Vecteur(v1.getVitx() + v2.getVitx(), v1.getVity() + v2.getVity());
 	}
-	
-	public static Vecteur somme(Vecteur...vecteurs) {
-		return somme(Arrays.asList(vecteurs));
-	}
-	
+
 	public static Vecteur somme(List<Vecteur> vecteurs) {
-		Vecteur res = new Vecteur(0,0);
+		Vecteur res = new Vecteur(0, 0);
 		for (Vecteur vecteur : vecteurs) {
 			res = Vecteur.somme(res, vecteur);
 		}
 		return res;
 	}
-	
+
 	public static Vecteur buildVector(Point p1, Point p2) {
-		return new Vecteur(p2.getX()-p1.getX(), p2.getY()-p1.getY());
+		return new Vecteur(p2.getX() - p1.getX(), p2.getY() - p1.getY());
+	}
+
+	public double getNorme() {
+		return Math.sqrt(vitx * vitx + vity * vity);
+	}
+
+	public Vecteur Produit(double k, Vecteur v) {
+		v.setVitx(v.getVitx() * k);
+		v.setVity(getVity() * k);
+		return v;
 	}
 	
-	public double getNorme() {
-		return Math.sqrt(vitx*vitx+vity*vity);
+	public Vecteur Division(double k) {
+		this.setVitx(this.getVitx() / k);
+		this.setVity(this.getVity() / k);
+		return this;
 	}
 
 	public void changeNorme(double norme) {
@@ -91,5 +105,4 @@ public class Vecteur {
 	public String toString() {
 		return "Vecteur [vitx=" + vitx + ", vity=" + vity + "]";
 	}
-	
 }
